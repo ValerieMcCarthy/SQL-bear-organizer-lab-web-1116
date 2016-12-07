120b7e1f3450ddd7cb8b69da01e7774c211b590e
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-12-07 15:56:24 -0500

    Bear table built and killer of Tim ID'ed.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 5, "M", "Brown", "Whoozy", 1); 
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 1, "M", "Black and White", "Crazy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 9, "F", "White", "Calm", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 7, "M", "Black", "Militant", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 3, "F", "Orange", "Rebelious", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 14, "M", "Green", "Comatose", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 4, "F", "Ecru", "Mellow", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (null, 8, "M", "Blond", "Angry", 1);
